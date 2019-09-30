5bb5f42035a92d48a75b84a2bbf5ba0004ee6a8f
prasad <prasdjes@gmail.com>
2019-09-30 10:37:31 -0700

    End of section 7

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -17,12 +17,12 @@ namespace DatingApp.API.Data
         {
             var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == username);
             if(user == null)
-            return null;
+                return null;
 
-             if(!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
-             return null;
+            if(!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+                return null;
 
-             return user;
+            return user;
 
         }
 
